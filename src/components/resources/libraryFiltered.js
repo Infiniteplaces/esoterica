d27059d79c9c87375d73e10f69ef35b36c902070
Tucker <tuckerschoos@gmail.com>
2020-05-05 18:35:01 -0400

    fix

--- src/components/resources/libraryFiltered.js
+++ src/components/resources/libraryFiltered.js
@@ -11,12 +11,14 @@ const LibraryFiltered = ({ posts, mobile }) => {
       <Col key={idx} xs="6">
         <div className="post-container">
           <Link to={"/resources/library/" + node.slug}>
-            <div
-              className="container-image"
-              style={{
-                backgroundImage: `url(${node.heroImage.fluid.src})`,
-              }}
-            />
+            <div className="wrapper">
+              <div
+                className="container-image"
+                style={{
+                  backgroundImage: `url(${node.heroImage.fluid.src})`,
+                }}
+              />
+            </div>
             <div className="post-text-container">
               <div className="tag-container">
                 {node.tags.length > 0
