9bb9e35a72e4cb7d1d2206c22a94e57a3d9089b5
Tucker <tuckerschoos@gmail.com>
2020-06-23 11:04:42 -0400

    new page update

--- src/pages/our-solutions/our-newest-fund.js
+++ src/pages/our-solutions/our-newest-fund.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link, useStaticQuery } from "gatsby"
 import get from "lodash/get"
+import { connect } from "react-redux"
 import { Container, Row, Col } from "reactstrap"
 import Img from "gatsby-image"
 
@@ -19,7 +20,30 @@ import phone_white from "../../images/icons/phone-white.svg"
 import mail_black from "../../images/icons/mail.svg"
 import mail_white from "../../images/icons/mail-white.svg"
 
+import pillar_one from "../../images/pages/pillars_one.png"
+import pillar_two from "../../images/pages/pillars_two.png"
+import pillar_three from "../../images/pages/pillars_three.png"
+import pillar_four from "../../images/pages/pillars_four.png"
+
+import number_one from "../../images/pages/number_one.svg"
+import number_two from "../../images/pages/number_two.svg"
+import number_three from "../../images/pages/number_three.svg"
+import number_four from "../../images/pages/number_four.svg"
+
+import arrow from "../../images/pages/right-arrow-heavy.svg"
+
 class OurNewestFundPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mobile: false,
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      mobile: window.innerWidth <= 1024,
+    })
+  }
   render() {
     const library = get(this, "props.data.allContentfulLibrary.edges")
     const featured = library.filter(i => i.node.featured === true)
@@ -37,21 +61,22 @@ class OurNewestFundPage extends React.Component {
           <Container fluid>
             <Row className="header-row">
               <Col>
-                <h2>
-                  5G is much more than an upgrade to wireless cellular networks.
-                </h2>
                 <p>
-                  We believe that the most value creation is not with the
-                  carriers – notwithstanding what their public pronouncements
-                  may imply. Value resides with the transformative technologies
-                  that are creating a new generation of semiconductors that
-                  power 5G; a new cloud computing architecture that’s moving to
-                  the edge to optimize the benefits of faster speeds, latency,
-                  capacity; software to filter and process vast quantities of
-                  data in real-time; and, enabling technologies such as
-                  streaming, smart homes, smart factories, remote healthcare,
-                  autonomous vehicles, AR/VR - and applications that have
-                  yet-to-be-created.
+                  5G is much more than an upgrade to wireless cellular networks;
+                  5G accelerates global digital transformation. We believe that
+                  value creation goes far beyond carriers – notwithstanding what
+                  their public pronouncements may imply. Value resides with the
+                  transformative technologies that are creating a new generation
+                  of semiconductors that power 5G and other advanced computing
+                  needs; a new cloud computing architecture that’s moving to the
+                  edge to optimize the benefits of faster speed, low latency,
+                  and massive capacity; software to filter and process vast
+                  quantities of data in real-time; and, enabling technologies
+                  such as streaming, smart homes, smart factories, remote
+                  healthcare, autonomous vehicles, AR/VR - and applications that
+                  have yet-to-be-created. 4G was foundational to mobile internet
+                  that has transformed our lives; 5G is digitalizing our economy
+                  at every level. 
                 </p>
                 <p>
                   Our investment process is underpinned by fundamental research,
@@ -68,42 +93,95 @@ class OurNewestFundPage extends React.Component {
                 </p>
                 <ul>
                   <li>
-                    <strong>How to capture the 5G opportunities? </strong>
-                    First, think globally – 4G was largely the U.S. focused; 5G
+                    <strong>How to capture the 5G opportunity? </strong>
+                    First, think globally – 4G was largely U.S. focused; 5G
                     adoption will be global from the outset. Second, think
                     top-down – allocate dynamically to “5G sectors” described
-                    below, as the technology evolves. Third, think bottom-up –
+                    below, as technology evolves. Third, think bottom-up –
                     invest in fundamentally superior companies within each “5G
-                    sector”. 
+                    sector”.
                   </li>
                   <li>
                     <strong>
-                      5G is not a light-switch; it is an evolution.{" "}
-                    </strong>
+                      5G is not a light-switch; it is an evolution.
+                    </strong>{" "}
                     We categorize the impact of 5G technology in four main
                     categories/sectors: (1) New Semiconductors, (2) Cloud & Edge
                     Computing, (3) SaaS & (4) Enabling Technology. We invest in
-                    all of the “sectors,” with allocations adjusted in-line with
-                    the development of the 5G cycle.
+                    all of these “sectors,” with allocations adjusted in-line
+                    with the development of the 5G cycle.
                   </li>
-                  <li>
-                    <strong>Semis are the nucleus of the 5G ecosystem. </strong>
-                    Every existing device and every facet of the 5G cycle has to
+                </ul>
+              </Col>
+            </Row>
+            <Row className="pillars-row">
+              <Col className="text-center border-top border-grey mx-md-5">
+                <h2>Our Pillars</h2>
+              </Col>
+              <Row className="pillar mx-md-5">
+                {this.state.mobile && (
+                  <Col xs="12" md="3" className="image-col">
+                    <img src={pillar_one} alt="" />
+                  </Col>
+                )}
+                <Col xs="2" md="1" className="number-col">
+                  <img src={number_one} alt="" />
+                </Col>
+                <Col xs="10" md="8" className="info-col pl-0">
+                  <h4>Semis are the nucleus of the 5G ecosystem.</h4>
+                  <p>
+                    Every existing device and every facet of the 5G cycle must
                     be upgraded with new semiconductor technology to run faster
-                    and smarter, all while being more energy-efficient. 5G will
-                    require a massive expansion of semiconductor content.
-                  </li>
-                  <li>
-                    <strong>Cloud giants push into Edge Computing. </strong>
-                    Edge computing and 5G are interlinked technologies as both
+                    and smarter, all the while being more energy efficient. 5G
+                    will require a massive expansion of semiconductor content.
+                  </p>
+                  <Link to="/resources/library/semi-eating-world/" className="">
+                    Semiconductors Eating the World <img src={arrow} alt="" />
+                  </Link>
+                </Col>
+                {!this.state.mobile && (
+                  <Col xs="12" md="3" className="image-col">
+                    <img src={pillar_one} alt="" />
+                  </Col>
+                )}
+              </Row>
+              <Row className="pillar mx-md-5 pb-md-5">
+                <Col xs="12" md="3" className="image-col">
+                  <img src={pillar_two} alt="" />
+                </Col>
+                <Col xs="2" md="1" className="number-col">
+                  <img src={number_two} alt="" />
+                </Col>
+                <Col xs="10" md="8" className="info-col pl-0">
+                  <h4>Cloud giants push into Edge Computing.</h4>
+                  <p>
+                    Edge Computing and 5G are interlinked technologies as both
                     are poised to significantly improve the performance of
                     applications (faster speed) and enable huge amounts of data
                     to be processed in real-time (low latency). Applications
                     running on edge locations demand lower latency and reduced
                     bandwidth.
-                  </li>
-                  <li>
-                    <strong>What does software have to do with 5G? </strong>
+                  </p>
+                  <Link
+                    to="/resources/library/the-convergence-of-cloud-and-5g/"
+                    className=""
+                  >
+                    The Convergence of Cloud and 5G <img src={arrow} alt="" />
+                  </Link>
+                </Col>
+              </Row>
+              <Row className="pillar mx-md-5">
+                {this.state.mobile && (
+                  <Col xs="12" md="3" className="image-col">
+                    <img src={pillar_three} alt="" />
+                  </Col>
+                )}
+                <Col xs="2" md="1" className="number-col">
+                  <img src={number_three} alt="" />
+                </Col>
+                <Col xs="10" md="8" className="info-col pl-0">
+                  <h4>What does software have to do with 5G?</h4>
+                  <p>
                     5G will result in a massive amount of new data. The ability
                     to continuously filter and process this new data, in
                     real-time, will be critical, particularly for A.I. and
@@ -111,20 +189,35 @@ class OurNewestFundPage extends React.Component {
                     platforms, big data analytics, and agile software
                     development tools are emerging to help individuals and
                     businesses build digital experiences in the context of
-                    5G/edge computing.
-                  </li>
-                  <li>
-                    <strong>Enabling technologies. </strong>
-                    Eventually, once all of the 5G building blocks are in place,
+                    5G/Edge Computing.
+                  </p>
+                </Col>
+                {!this.state.mobile && (
+                  <Col xs="12" md="3" className="image-col">
+                    <img src={pillar_three} alt="" />
+                  </Col>
+                )}
+              </Row>
+              <Row className="pillar mx-md-5">
+                <Col xs="12" md="3" className="image-col">
+                  <img src={pillar_four} alt="" />
+                </Col>
+                <Col xs="2" md="1" className="number-col">
+                  <img src={number_four} alt="" />
+                </Col>
+                <Col xs="10" md="8" className="info-col pl-0">
+                  <h4>Enabling Technologies.</h4>
+                  <p>
+                    Eventually, when all of the 5G building blocks are in place,
                     the big winners will be the enabling technologies (think of
                     Instagram and Netflix in the 4G cycle), giving rise to a
                     wave of new 5G business applications, devices, and services:
                     smart homes, smart factories, remote healthcare, autonomous
                     vehicles, AR/VR - and applications that have
                     yet-to-be-created.
-                  </li>
-                </ul>
-              </Col>
+                  </p>
+                </Col>
+              </Row>
             </Row>
             <Row className="contact-row">
               <Col className="call-cta">
@@ -169,7 +262,12 @@ class OurNewestFundPage extends React.Component {
   }
 }
 
-export default OurNewestFundPage
+export default connect(
+  state => ({
+    mobile: state.global.mobile,
+  }),
+  null
+)(OurNewestFundPage)
 
 export const OurNewestFundQuery = graphql`
   query OurNewestFundQuery {
